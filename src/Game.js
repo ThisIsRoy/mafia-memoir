a985e1e82e5eefa9f7c5fb83cb2491081da918cd
Roy Sun <rs58@rice.edu>
2020-08-14 11:16:17 -0500

    first commit

---
+++ src/Game.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import App from './App';
+
+class Game extends Component {
+    render() {
+        return (
+            <div className="Game">
+                <div className="MafiaPlayers">
+
+                </div>
+                <div className="TownPlayers">
+
+                </div>
+            </div>
+        )
+    };
+}
+
+export default Game; 
