53695ba90be20e62173f022d6d2f5490305d2167
Roy Sun <rs58@rice.edu>
2020-09-06 18:29:53 -0500

    styled some of new game nav bar

--- src/NewGameForm.js
+++ src/NewGameForm.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
 import clsx from 'clsx';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import Button from '@material-ui/core/Button';
@@ -32,23 +27,6 @@ const drawerWidth = 400;
 const useStyles = makeStyles((theme) => ({
     root: {
       display: 'flex',
-    },
-    appBar: {
-      transition: theme.transitions.create(['margin', 'width'], {
-        easing: theme.transitions.easing.sharp,
-        duration: theme.transitions.duration.leavingScreen,
-      }),
-    },
-    appBarShift: {
-      width: `calc(100% - ${drawerWidth}px)`,
-      marginLeft: drawerWidth,
-      transition: theme.transitions.create(['margin', 'width'], {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
     },
     hide: {
       display: 'none',
@@ -192,7 +170,6 @@ export default function NewGameForm(props) {
     return (
       <div className={classes.root}>
         <NewGameFormNav 
-          classes={classes}
           open={open}
           handleDrawerOpen={handleDrawerOpen}
           createGame={createGame}
--- src/NewGameFormNav.js
+++ src/NewGameFormNav.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import clsx from 'clsx';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
@@ -7,17 +7,44 @@ import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Button from '@material-ui/core/Button';
- 
+import {withStyles} from '@material-ui/core/styles';
+
+const styles = theme => ({
+    root: {
+        display: "flex"
+    },
+    appBar: {
+        transition: theme.transitions.create(['margin', 'width'], {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.leavingScreen,
+        }),
+        flexDirection: "row",
+        justifyContent: "space-between"
+      },
+      appBarShift: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+        transition: theme.transitions.create(['margin', 'width'], {
+          easing: theme.transitions.easing.easeOut,
+          duration: theme.transitions.duration.enteringScreen,
+        }),
+      },
+      menuButton: {
+        marginRight: theme.spacing(2),
+    },
+});
+
+const drawerWidth = 400; 
+
 class NewGameFormNav extends Component {
     constructor(props) {
         super(props);
     }
 
     render() {
-        const {classes, open, handleDrawerOpen, createGame, gameName, players, hasAssignedRoles} = this.props;
-
+        const { classes, open, handleDrawerOpen, createGame, gameName, players, hasAssignedRoles } = this.props;
         return(
-            <div>
+            <div className={classes.root}>
                 <CssBaseline />
                 <AppBar
                     color="default"
@@ -25,8 +52,7 @@ class NewGameFormNav extends Component {
                     className={clsx(classes.appBar, {
                         [classes.appBarShift]: open,
                     })}
-                >
-
+                > 
                     <Toolbar>
                         <IconButton
                             color="inherit"
@@ -37,7 +63,9 @@ class NewGameFormNav extends Component {
                         >
                             <MenuIcon />
                         </IconButton>
-                
+                    </Toolbar>
+
+                    <div className={classes.navButtons}>
                         <Button variant="contained" 
                             color="primary" 
                             onClick={createGame}
@@ -50,12 +78,11 @@ class NewGameFormNav extends Component {
                                 Home
                             </Button>
                         </Link>
-                    </Toolbar>
-
+                    </div>
                 </AppBar>
             </div>
         )
     }
 }
 
-export default NewGameFormNav;
+export default withStyles(styles, { withTheme: true})(NewGameFormNav);
